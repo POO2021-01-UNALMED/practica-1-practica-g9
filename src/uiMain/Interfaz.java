27d16cdf73e6953db0a5d6994864af97e43d826b
Kevin <kjayh@unal.edu.co>
2021-04-18 13:21:44 -0500

    interfaz comienzo

---
+++ src/uiMain/Interfaz.java
@@ -0,0 +1,40 @@
+package uiMain;
+import java.util.Scanner;
+
+public class Interfaz {
+    public static Scanner input = new Scanner(System.in);
+    public static void main(String[] args){
+    String option;
+
+
+    label:
+            while (true) {
+
+        System.out.println("-----------------------------");
+        System.out.println("Bienvenido ");
+        System.out.println("Escoja una opcion:");
+        System.out.println("1. Ingresar usuario");
+        System.out.println("2. Registrar usuario");
+        System.out.println("0. Salir");
+        System.out.println("-----------------------------");
+        System.out.println();
+        option = input.next();
+        switch (option) {
+            case "1":
+                //ingresarUsuario();
+
+
+                break;
+            case "2":
+                //registrarUsuario();
+
+
+                break;
+            case "0":
+                break label;
+        }
+    }
+
+
+}
+}
