bb4da54496246053994f88b32700282cbf67b461
Kevin <kjayh@unal.edu.co>
2021-04-18 13:52:02 -0500

    personas

--- src/gestorAplicacion/Persona.java
+++ src/gestorAplicacion/Persona.java
@@ -4,4 +4,11 @@ public class Persona {
     String nombre;
     int edad;
     int documento;
+
+    public Persona(String nombre, int edad, int documento){
+        this.nombre = nombre;
+        this.edad = edad;
+        this.documento = documento;
+
+    }
 }
