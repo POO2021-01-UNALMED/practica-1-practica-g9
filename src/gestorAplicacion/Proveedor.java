0796814e4618ad5378a6be029f62f0ba7852ff1e
Omar <ochavez@unal.edu.co>
2021-04-22 17:50:46 -0500

    Proveedor y medicamento
    Ahora si

--- src/gestorAplicacion/Proveedor.java
+++ src/gestorAplicacion/Proveedor.java
@@ -1,9 +1,27 @@
 package gestorAplicacion;
 
 public class Proveedor {
-
-	public Proveedor() {
-		// TODO Auto-generated constructor stub
+	private int NIT;
+	private String nombreprov; // nombre proveedor
+	private int telefonoprov; // telefono del proveedor
+	private String Dirprov; //direccion del proveedor
+	public Proveedor(int NIT, String nombre, int telefono, String direccion) {
+		this.NIT = NIT;
+		nombreprov = nombre;
+		telefonoprov = telefono;
+		Dirprov = direccion;
 	}
 
+	public void setNIT(int NIT) {
+		this.NIT = NIT;
+	}
+	public int getNIT() {
+		return this.NIT;
+	}
+	public void setNombreProv(String nombre) {
+		nombreprov = nombre;
+	}
+	public String getNombreprov() {
+		return nombreprov;
+	}
 }
