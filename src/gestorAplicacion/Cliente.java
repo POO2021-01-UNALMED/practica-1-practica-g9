2d3f25719ba131bc242259db96c8fa794eb1cdd8
Omar <ochavez@unal.edu.co>
2021-04-22 16:30:44 -0500

    creacion de cliente

--- src/gestorAplicacion/Cliente.java
+++ src/gestorAplicacion/Cliente.java
@@ -1,9 +1,25 @@
 package gestorAplicacion;
 
-public class Cliente {
-
-	public Cliente() {
-		// TODO Auto-generated constructor stub
+public class Cliente extends Persona {
+	private String direccion;
+	private int telefono;
+	public Cliente(String nombre, int edad, int documento, String direccion, int telefono) {
+		this.setNombre(nombre);
+        this.setEdad(edad);
+        this.setDocumento(documento);
+        this.direccion = direccion;
+		this.telefono = telefono;
 	}
-
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	public void setTelefono(int telefono) {
+		this.telefono = telefono;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public int getTelefono() {
+		return telefono;
+	}
 }
