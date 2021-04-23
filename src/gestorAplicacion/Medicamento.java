fb4f657f30bf5b7d8cc8946ff974a29fdbc2dfac
Omar <ochavez@unal.edu.co>
2021-04-23 12:29:28 -0500

    Medicamento
    Ahora sí

--- src/gestorAplicacion/Medicamento.java
+++ src/gestorAplicacion/Medicamento.java
@@ -1,9 +1,36 @@
 package gestorAplicacion;
 
 public class Medicamento {
-
-	public Medicamento() {
-		// TODO Auto-generated constructor stub
+	private final int codigo;
+	private String nombre;
+	private Proveedor prov;
+	private int cant; //cantidad del medicamento en el inventario
+	public Medicamento(int codigo, String nombre, Proveedor prov, int cant) {
+		this.codigo = codigo;
+		this.nombre = nombre;
+		this.prov = prov;
+		this.cant = cant;
 	}
 
+	public int getCodigo() {
+		return this.codigo;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setProveedor(Proveedor prov) {
+		this.prov = prov;
+	}
+	public Proveedor getProveedor() {
+		return this.prov;
+	}
+	public void setcant(int cant) {
+		this.cant = cant;
+	}
+	public int getCantidad() {
+		return cant;
+	}
 }
